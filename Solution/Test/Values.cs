5e8fa19ba957375369ee105bb58922b8376dcb1b
bestserg <sergeyglad@mail.ru>
2014-05-10 17:23:12 +0400

    merge

--- Solution/Test/Values.cs
+++ Solution/Test/Values.cs
@@ -18,34 +18,57 @@ namespace Test
         //{ get { return _numberWithField; } set { _numberWithField = value; } }
 
         [SecureField]
-        public float Number 
-        //{ 
-        //    get { return _number; } 
-        //    set { _number = value; } 
+        public int Number { get; set; }
+        //{
+        //    get
+        //    {
+        //        var currentMethod = MethodBase.GetCurrentMethod();
+        //        var builder = SecureFieldBuilder.Factory();
+        //        return (int)builder.GetSecureField(currentMethod, this);
+        //    }
+        //    set
+        //    {
+        //        var currentMethod = MethodBase.GetCurrentMethod();
+        //        var builder = SecureFieldBuilder.Factory();
+        //        builder.SetSecureField(currentMethod, this, (object)value);
+        //    }
         //}
-        {
-            get
-            {
-                var currentMethod = MethodBase.GetCurrentMethod();
-                var sf = SecureFieldBuilder.Factory();
-                return sf.GetSecureField(currentMethod, this);
-            }
-            set
-            {
-                var currentMethod = MethodBase.GetCurrentMethod();
-                var sf = SecureFieldBuilder.Factory();
-                sf.SetSecureField(currentMethod, this, value);
-            }
-        }
+    }
+}
 
+/* SetMethod original
+set
+{
+    MethodBase currentMethod = MethodBase.GetCurrentMethod();
+    SecureFieldBuilder.Factory().SetSecureField(currentMethod, this, value);
+}
+ * IL:
+.method public hidebysig specialname instance void set_Number(int32 'value') cil managed
+{
+    .maxstack 4
+    .locals init (
+        [0] class [mscorlib]System.Reflection.MethodBase currentMethod)
+    L_0000: nop 
+    L_0001: call class [mscorlib]System.Reflection.MethodBase [mscorlib]System.Reflection.MethodBase::GetCurrentMethod()
+    L_0006: stloc.0 
+    L_0007: call class [SecureField]SecureField.SecureFieldBuilder [SecureField]SecureField.SecureFieldBuilder::Factory()
+    L_000c: ldloc.0 
+    L_000d: ldarg.0 
+    L_000e: ldarg.1 
+    L_000f: box int32
+    L_0014: callvirt instance void [SecureField]SecureField.SecureFieldBuilder::SetSecureField(class [mscorlib]System.Reflection.MethodBase, object, object)
+    L_0019: nop 
+    L_001a: ret 
+}
 
+ 
 
  * GetMethod original
 get
 {
     MethodBase currentMethod = MethodBase.GetCurrentMethod();
     return SecureFieldBuilder.Factory().GetSecureField(currentMethod, this);
-    }
+}
  * IL:
 .method public hidebysig specialname instance int32 get_Number() cil managed
 {
